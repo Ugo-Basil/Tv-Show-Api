228eaef1f1c6b2936cb28b0e402db01a5610da32
Basil Ohuruogu <ohuruogubasil@gmail.com>
2022-10-01 10:45:27 +0100

    searchbar:1.0.0

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import {useState } from 'react'
 
 export default function Searchbar() {
+  const [searchTerm, setSearchTerm] = useState('')
+  
+  const onSearchHandler = (e) => {
+    e.preventDefault();
+    
+    console.log('Searching for term' + searchTerm);
+  }
+
   return (
-    <div className='searchbar'> This is the Searchbar</div>
+    <div className='searchbar'>
+      <form className='searchbar__form'>
+        <input type="text" placeholder='Search For Tv Show' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+        
+        <button className='btn btn-block' onClick={onSearchHandler}>Search</button>
+      </form> 
+     
+    
+    </div>
   )
 }
